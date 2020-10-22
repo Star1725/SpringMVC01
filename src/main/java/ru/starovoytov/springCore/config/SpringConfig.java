d49b52e826768933dd27d07bca44eb1957e348ce
Sergey <starovoytov1725@gmail.com>
2020-10-22 18:02:20 +0300

    созданы java-config файлы

---
+++ src/main/java/ru/starovoytov/springCore/config/SpringConfig.java
@@ -0,0 +1,9 @@
+package ru.starovoytov.springCore.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.starovoytov.springCore")
+public class SpringConfig {
+}
