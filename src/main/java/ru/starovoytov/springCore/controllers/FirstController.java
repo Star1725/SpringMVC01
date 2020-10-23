87c54049d14c95b424e5bda2333fb0f749c3ce0e
Sergey <starovoytov1725@gmail.com>
2020-10-23 22:29:04 +0300

    добавили FirstController @RequestMapping("/first")

--- src/main/java/ru/starovoytov/springCore/controllers/FirstController.java
+++ src/main/java/ru/starovoytov/springCore/controllers/FirstController.java
@@ -2,8 +2,10 @@ package ru.starovoytov.springCore.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+//@RequestMapping("/first")//данная аннотация добаыляет в методы к запросу ещё "/first" и получаем - "/first/hello" и "/first/goodbye"
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(){
