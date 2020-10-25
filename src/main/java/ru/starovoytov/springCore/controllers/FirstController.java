b907022775a29c64a7c991e3fc08df5b02703c9a
Sergey <starovoytov1725@gmail.com>
2020-10-25 11:04:10 +0300

    required = false - позволяет доставать value = "name", где name будет равен null

--- src/main/java/ru/starovoytov/springCore/controllers/FirstController.java
+++ src/main/java/ru/starovoytov/springCore/controllers/FirstController.java
@@ -11,8 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 //@RequestMapping("/first")//данная аннотация добаыляет в методы к запросу ещё "/first" и получаем - "/first/hello" и "/first/goodbye"
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(@RequestParam ("name") String name,
-                            @RequestParam("surname") String surname){
+    public String helloPage(@RequestParam (value = "name", required = false) String name,//required = false - позволяет доставать value = "name", где name будет равен null
+                            @RequestParam(value = "surname", required = false) String surname){
 
         System.out.println("Hello " + name + " " + surname);
 
